acec5c0f408352e315489f109a294409274b2d6f
ch1aki <sugawara256@gmail.com>
2019-07-20 10:11:50 +0900

    add alacritty

--- roles/arch/default.rb
+++ roles/arch/default.rb
@@ -1,4 +1,5 @@
 include_cookbook 'symboliclinks'
+include_cookbook 'yay'
 include_cookbook 'git'
 include_cookbook 'vim'
 include_cookbook 'fzf'
@@ -15,16 +16,9 @@ if ask 'install Python'
   include_cookbook 'python'
 end
 
-if ask 'install Rust'
-  include_cookbook 'rust'
-end
-
-ln '.bash_profile'
-ln '.xprofile'
-
 include_cookbook 'tmux'
 include_cookbook 'fish'
-include_cookbook 'yay'
+include_cookbook 'alacritty'
 
 yay 'ttf-cica' do
   action :install
