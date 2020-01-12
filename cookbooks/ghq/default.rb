355ece58cbd9dbe15c0d8792443ee15bda6376c8
ch1aki <sugawara256@gmail.com>
2020-01-12 20:55:32 +0900

    fix

--- cookbooks/ghq/default.rb
+++ cookbooks/ghq/default.rb
@@ -1,8 +1,8 @@
 github_binary 'ghq' do
   repository 'motemen/ghq'
   version 'v0.10.0'
-  archive "ghq_#{node[:os]}_amd64.zip"
-  binary_path "ghq_#{node[:os]}_amd64/ghq"
+  archive "ghq_#{node[:platform]}_amd64.zip"
+  binary_path "ghq_#{node[:platform]}_amd64/ghq"
 end
 
 include_cookbook 'git'
