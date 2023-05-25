4db769a38b4af05ec4d4358abbd41ad26fb92826
akichan <sugawara@pepabo.com>
2023-05-25 22:13:02 +0900

    add lazygit

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -27,4 +27,14 @@ return {
       })
     end
   },
+  {
+      "kdheepak/lazygit.nvim",
+      dependencies = {
+          "nvim-lua/plenary.nvim",
+      },
+      config = function ()
+        vim.g.lazygit_floating_window_winblend = 0
+        vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>")
+      end
+  },
 }
